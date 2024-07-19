c73501584f62159db54e3c3cf7fd694e4ca151f6
BillyTan <www.billy39@gmail.com>
2024-07-19 15:12:47 +0700

    add utils to show hide element

--- src/Utils.js
+++ src/Utils.js
@@ -1,6 +1,14 @@
 class Utils {
-  static clearElement(targetElement) {
-    targetElement.innerHTML = '';
+  static clearElement(element) {
+    element.innerHTML = '';
+  }
+  static hideElement(element) {
+    element.style.display = 'none';
+    element.hidden = true;
+  }
+  static showElement(element) {
+    element.style.display = 'block';
+    element.hidden = false;
   }
 }
 
